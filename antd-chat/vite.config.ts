18356656a2e3578f34ba5077b4326b82c317c20e
wu suncoo <a123648305@126.com>
2024-04-19 14:44:21 +0800

    feat: 改用pm2 管理应用

--- antd-chat/vite.config.ts
+++ antd-chat/vite.config.ts
@@ -8,4 +8,7 @@ export default defineConfig({
     UnoCSS(),
     react(),
   ],
+  build: {
+    outDir: "../dist",
+  },
 });
---
+++ ecosystem.config.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      name: "chatRoom_Server",
+      script: "./index.js",
+      args: "--attach",
+      error_file: "./log/err.log", // 错误日志
+      out_file: "./log/out.log", // 输出日志
+      log_date_format: "YYYY/MM/DD HH:mm:ss", // 日志日期格式
+    },
+    {
+      name: "chatRoom_Client",
+    //   cwd: "./antd-chat",
+      script: "serve", // 启动静态服务器
+      env: {
+        PM2_SERVE_PATH: './dist',
+        PM2_SERVE_PORT: 8080,
+        PM2_SERVE_SPA: 'true',
+        PM2_SERVE_HOMEPAGE: '/index.html'
+      }
+    },
+  ],
+};
